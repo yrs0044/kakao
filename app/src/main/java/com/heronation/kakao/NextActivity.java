dd872b7b0218b6060a0612c966cbaaa046be4c52
HeroNation <yrs0044@naver.com>
2017-03-27 14:51:57 +0900

    170327_GitTest_kakao

---
+++ app/src/main/java/com/heronation/kakao/NextActivity.java
@@ -0,0 +1,62 @@
+package com.heronation.kakao;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.kakao.usermgmt.UserManagement;
+import com.kakao.usermgmt.callback.LogoutResponseCallback;
+
+/**
+ * Created by HeroNation on 2017-03-22.
+ */
+
+public class NextActivity extends AppCompatActivity {
+
+    TextView tv;
+    Button logout;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        logout = (Button)findViewById(R.id.logout_btn);
+        tv = (TextView) findViewById(R.id.tv_next);
+
+        Intent intent = new Intent();
+        String kakaoID =  intent.getStringExtra("KAKAO");
+        Log.i("asdsdfasdfasdf.....",kakaoID);
+        tv.setText(kakaoID);
+
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onClickLogout();
+            }
+        });
+
+    }
+
+    private void onClickLogout() {
+        UserManagement.requestLogout(new LogoutResponseCallback() {
+            @Override
+            public void onCompleteLogout() {
+                redirectLoginActivity();
+            }
+        });
+    }
+    protected void redirectLoginActivity() {       //세션 연결 성공 시 SignupActivity로 넘김
+        final Intent intent = new Intent(this, MainActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+        startActivity(intent);
+        finish();
+    }
+}
